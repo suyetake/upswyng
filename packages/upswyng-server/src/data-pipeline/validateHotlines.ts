a67f255c399d6d9fd51671a108c35a7aa92b2c7d
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2019-12-11 19:03:40 -0700

    include name of hotline in failed validatations

--- packages/upswyng-server/src/data-pipeline/validateHotlines.ts
+++ packages/upswyng-server/src/data-pipeline/validateHotlines.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 import { THotline } from "@upswyng/upswyng-types";
 
+type hotlineURL = {
+  name: string
+  url: string
+};
+
 axios
   .get("https://strapped.firebaseio.com/crisisline.json")
   // pull the source json back
@@ -12,15 +17,16 @@ axios
       string,
       THotline
     ][];
-  console.log(entries);
+    //console.log(entries);
 
     return entries;
   })
   // parse the json and get all URLs back
   .then(entries => {
+    const entryPrimaryKey: string = "name" 
     const urlKeys: string[] = ["website", "chatweblink"];
 
-    const validUrls: string[] = entries.reduce((acc, [_, hotline]) => {
+    const validUrls: hotlineURL[] = entries.reduce((acc, [_, hotline]) => {
       // foreach hotline, iterate through the sub-keys
 
       return acc.concat(
@@ -31,30 +37,30 @@ axios
           if (typeof hotline[key] == "string") {
             url = String(hotline[key]).trim();
           }
-          if (url.length > 0) innerAcc.push(url);
+          if (url.length > 0) innerAcc.push({name:hotline[entryPrimaryKey], url});
 
           return innerAcc;
 
-        }, new Array<string>())
+        }, new Array<hotlineURL>())
       );
 
-    }, new Array<string>());
+    }, new Array<hotlineURL>());
     return validUrls;
   })
-  .then(validUrls =>{
-    validUrls.map(url => {
-      if (!url.startsWith('http')) {
-        url = `https://${url}`
+  .then((hlURLs: hotlineURL[]) =>{
+    hlURLs.map((hlURL: hotlineURL )=> {
+      if (!hlURL.url.startsWith('http')) {
+        hlURL.url = `https://${hlURL.url}`
       }
       axios
-        .get(url)
+        .get(hlURL.url)
         .then(async ({ status, statusText, data: resources }) => {
-          console.log(url, status)
+          console.log(hlURL.url, status)
         })
         .catch(async (reason: any) => {
-          console.log(url, 'FAILED');
+
+          console.log(hlURL, 'FAILED');
         })
     })
-    console.log(validUrls);
   });
 
